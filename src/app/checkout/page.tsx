902257781d2cb1756f5f31ae1fed2fbff12119db
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-19 22:03:07 +0600

    cart delete functionality added

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Button } from "@mui/material";
+import Link from "next/link";
+import React from "react";
+
+function Checkout() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-4xl font-semibold">
+      <h2 className="mb-5">Checkout page is in under construction</h2>
+      <Button variant="contained" color="primary" className="bg-black">
+        <Link href="/"> Back to Home</Link>
+      </Button>
+    </div>
+  );
+}
+
+export default Checkout;
