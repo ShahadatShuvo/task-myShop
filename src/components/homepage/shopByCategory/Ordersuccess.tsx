3d4594acf51e731cb8561f75d80bb3a7439f0263
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-19 16:13:15 +0600

    Working on product card section[Snackbar added]

---
+++ src/components/homepage/shopByCategory/Ordersuccess.tsx
@@ -0,0 +1,31 @@
+"use-client";
+
+import { SnackbarProvider, VariantType, useSnackbar } from "notistack";
+import { IconButton } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import * as React from "react";
+
+function MyApp() {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleClickVariant = (variant: VariantType) => () => {
+    // variant could be success, error, warning, info, or default
+    enqueueSnackbar("Product Successfully Added to Cart!", { variant });
+  };
+
+  return (
+    <React.Fragment>
+      <IconButton aria-label="added" onClick={handleClickVariant("success")}>
+        <AddCircleOutlineIcon />
+      </IconButton>
+    </React.Fragment>
+  );
+}
+
+export default function OrderSuccess() {
+  return (
+    <SnackbarProvider maxSnack={3}>
+      <MyApp />
+    </SnackbarProvider>
+  );
+}
